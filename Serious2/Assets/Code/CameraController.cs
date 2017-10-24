0b5b3b3333963cfd16933421b7064c903dcf06f9
Mike Breese <Michael1Breese@gmail.com>
2017-10-23 22:10:52 -0600

    basic gameplay idea

---
+++ Serious2/Assets/Code/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject view;
+
+    float mouseWheel;
+    //bool leftClick;
+
+	// Use this for initialization
+	void Start () {
+        mouseWheel = Input.GetAxis("Mouse ScrollWheel");
+        //leftClick = Input.GetButton("Fire1");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (mouseWheel > 0f)
+        {
+            Debug.Log("Going up");
+            view.transform.position = new Vector3(view.transform.position.x, view.transform.position.y + 1,0f);
+        }
+        else if (mouseWheel < 0f)
+        {
+            Debug.Log("Going down");
+            view.transform.position = new Vector3(view.transform.position.x, view.transform.position.y - 1, 0f);
+        }
+    }
+}
---
+++ Serious2/Assets/Code/Controls.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controls : MonoBehaviour {
+
+    public Slider energy;
+
+    public Button inflammatoryHeadlineButton;
+    public int inflammatoryHeadlineCost;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void InflammatoryHeadlines(Text Headline)
+    {
+        Headline.text = "Comey is an Idiot who eats unbaptised babies!";
+        energy.value = energy.value - inflammatoryHeadlineCost;
+
+        //Disable button
+        inflammatoryHeadlineButton.interactable = false;
+    }
+
+    void checkToDisableButtons()
+    {
+
+    }
+}
